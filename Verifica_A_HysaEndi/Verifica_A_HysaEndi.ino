467f2b25f58f50dc381cad0a774296b9abbb05cc
Endi <endi.watch@gmail.com>
2019-02-16 08:27:04 +0100

    16/02/2019 -- dichiarazione variabili

---
+++ Verifica_A_HysaEndi/Verifica_A_HysaEndi.ino
@@ -0,0 +1,22 @@
+
+int rosso;      //dichiarazione variabili
+int giallo;
+int bianco;
+int verde;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  rosso = 12;
+  giallo = 6;
+  bianco = 8;
+  verde = 10;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
